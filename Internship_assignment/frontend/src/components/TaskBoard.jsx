871993dc0c109c5cb5e0ba4bf28e687ce008d315
Sarthak-1229 <sarthak.b.1204@gmail.com>
2026-06-16 19:36:21 +0530

    feat: render the three glassmorphism columns in TaskBoard

--- Internship_assignment/frontend/src/components/TaskBoard.jsx
+++ Internship_assignment/frontend/src/components/TaskBoard.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Column from './Column';
 import './TaskBoard.css';
 
 function TaskBoard() {
+  const columns = ['To Do', 'In Progress', 'Done'];
+
   return (
     <div className="task-board-container">
-      {/* Columns will go here */}
+      {columns.map((col) => (
+        <Column key={col} id={col} title={col} />
+      ))}
     </div>
   );
 }
